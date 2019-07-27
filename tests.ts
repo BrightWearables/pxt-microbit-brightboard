c183173f7ec8c98e8970e5fefce9e97eb89bcffa
GeekMomProjects <debraansell@yahoo.com>
2019-07-27 15:27:17 -0700

    fixed clear function - added test

---
+++ tests.ts
@@ -0,0 +1,19 @@
+input.onButtonPressed(Button.A, function () {
+    brightboard.setPixelArray(brightboard.colorForLed(0xff0000, 0xFF7F00, 0xFFFE00, 0x7FFF00, 0x00FF00, 0x00FF7F, 0x00FFFE, 0x007FFF, 0x0000FF, 0x7F00FF, 0xFE00FF, 0xFF007F))
+    brightboard.show()
+})
+input.onButtonPressed(Button.B, function () {
+    brightboard.clear()
+    brightboard.show()
+})
+basic.showIcon(IconNames.Heart)
+brightboard.setBrightness(64)
+brightboard.clear()
+brightboard.show()
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    brightboard.setPixelColor(Math.randomRange(1, 12), brightboard.randomColor())
+    brightboard.show()
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(500)
+})
